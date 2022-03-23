35d0089d1d1706f9a9491975b94834cc0e5762f2
clipeus-virtutis <henry_mcd_93@hotmail.com>
2022-03-23 11:50:13 +0000

    clearing up files

--- db/seed.js
+++ db/seed.js
@@ -13,13 +13,14 @@ const seed = async ({
   itemsData,
 }) => {
   //  drops the data base before initializing
+
   await dropDatabase();
 
   //  defines a 'schema' for each table which is a template for a collection (table)
-  const { Category, Charity, CharityRequirement, DonatorItem, Donator, Item } = await createCollections();
-  //console.log(Category);
 
-  //  creates the 'Charities' collection using the CharityUserScheme defined in create collections
+  const { Category, Charity, CharityRequirement, DonatorItem, Donator, Item } = await createCollections();
+
+  //  creates the collections (tables) using the schemas defined in create collections function and is passed the relevant data as an argument
 
   await Charity.create(charitiesUserData);
   await Category.create(categoriesData);
@@ -27,43 +28,6 @@ const seed = async ({
   await DonatorItem.create(donatorItemsData);
   await Donator.create(donatorUsersData);
   await Item.create(itemsData);
-
-  // await CharityRequirement.insertMany(charityReqsData);
-
-  // donatorItemsData.forEach((donatorItems) => {
-  // 	DonatorItem.create(donatorItems, (err, instanceDonorItems) => {
-  // 		console.log(instanceDonorItems);
-  // 	});
-  // });
-
-  // donatorUsersData.forEach((donor) => {
-  // 	Donator.create(donor, (err, instanceDonor) => {
-  // 		console.log(instanceDonor);
-  // 	});
-  // });
-
-  // itemsData.forEach((item) => {
-  // 	Item.create(item, (err, instanceItem) => {
-  // 		console.log(instanceItem);
-  // 	});
-  // });
 };
 
 module.exports = seed;
-
-//  creates a instance of the charity model (a document (record) in a collection)
-
-// const testInstance = new Charity({
-// 	charityName: 'Charity 1',
-// 	address: '1 charity road, location1, A666AA',
-// 	charityWebsite: 'testcharitywebsite1',
-// 	charityusername: 'CharityUser1',
-// 	password: 'TestCharityPassword1',
-// 	emailAddress: 'testEmail1',
-// });
-
-// //  saves the document (record) to the Charities collection (table) defined by the model in line 25
-
-// testInstance.save(() => {
-// 	db.close();
-// });
