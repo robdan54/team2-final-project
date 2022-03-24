f36a20e9972db1cad9f43f405ca6d8f5ddfa1c22
clipeus-virtutis <henry_mcd_93@hotmail.com>
2022-03-24 12:35:08 +0000

    implements error handling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const { getDonors, getCharities } = require('./controllers/donor-controllers');
+const { handlesCustomErrors, handlesPsqlErrors, handles500Errors } = require('./controllers/error-controller');
 
 const app = express();
 
@@ -11,5 +12,13 @@ app.use(express.json());
 app.get('/api/donors', getDonors);
 
 app.get('/api/charities', getCharities);
+
+app.all('/*', (req, res) => {
+  res.status(404).send({ msg: 'Path not found' });
+});
+
+app.use(handlesCustomErrors);
+app.use(handlesPsqlErrors);
+app.use(handles500Errors);
 
 module.exports = app;
---
+++ controllers/error-controller.js
@@ -0,0 +1,20 @@
+exports.handlesCustomErrors = (err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
+};
+
+exports.handlesPsqlErrors = (err, req, res, next) => {
+  if (err.code === '22P02') {
+    res.status(400).send({ msg: 'Bad Request - incorrect body data' });
+  } else if (err.code === '23503') {
+    res.status(400).send({ msg: "Bad Request - username doesn't exist" });
+  } else if (err.code === '23502') {
+    res.status(400).send({ msg: 'Bad Request - invalid input' });
+  } else next(err);
+};
+
+exports.handles500Errors = (err, req, res) => {
+  console.log(err, 'err');
+  res.status(500).send({ msg: 'Server Error' });
+};
