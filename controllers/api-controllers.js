4d429f32c27b2144ab3ef5814e1ed6b4afc220f9
clipeus-virtutis <henry_mcd_93@hotmail.com>
2022-03-25 11:18:15 +0000

    refactoring get-api endpoint

--- controllers/api-controllers.js
+++ controllers/api-controllers.js
@@ -1,7 +1,7 @@
 const { fetchEndpoints } = require('../models/api-models');
 
 exports.getApi = (req, res, next) => {
-  fetchEndpoints
+  fetchEndpoints()
     .then((endpoints) => {
       res.status(200).send({ endpoints });
     })
