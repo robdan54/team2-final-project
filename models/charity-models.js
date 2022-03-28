f58ab4b911a7aa7ad0ee6a183b71aa235488d55f
clipeus-virtutis <henry_mcd_93@hotmail.com>
2022-03-28 10:18:18 +0100

    commenting

--- models/charity-models.js
+++ models/charity-models.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt');
 const db = require('../db/connection');
 
+//  returns charity users info
+
 exports.fetchCharities = () => db.query('SELECT charity_id, charity_name, address, charity_website, email_address, lat, lng FROM charities_users;').then((result) => result.rows);
 
 exports.postCharity = async (charity) => {
